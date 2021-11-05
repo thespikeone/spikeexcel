7b7b1fb6e1a1524488fb1f72bc5b2cef9fb83040
THESPIKEONE <ysarni42@gmail.com>
2021-11-05 20:41:38 +0100

    verif_mail change in email

--- assets/php/verif/email_verif.php
+++ assets/php/verif/email_verif.php
@@ -31,54 +31,12 @@ if(!empty($vkey)){
     </head>
     <body>
     <style>
-        body{
-        background-color: #e1e1e1;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-    .container{
-      max-width: 680px;
-      width: 100%;
-      margin: auto;
-    }
-    main{
-        margin: 0 auto;
-        display: flex;
-        flex-direction: column;
-        color: #555555; 
-    }
-    .body h2{
-        font-weight: 300;
-        color: #464646;
-    }
+        
     .logo{
         width: 150px;
         padding: 5px 5px;
     }
-    a{
-        text-decoration: underline; 
-        color: #0c99d5; 
-    }
-    .body{
-        padding: 20px;
-        background-color: white;
-        font-family: Geneva, Tahoma, Verdana, sans-serif; 
-        font-size: 16px; 
-        line-height: 22px; 
-        color: #555555; 
-    }
-    button{
-        background-color: #0c99d5;
-        border: none;
-        color: white;
-        border-radius: 2px;
-        height: 50px;
-        max-width: 250px;
-       padding: 0px 30px;
-        font-weight: 500;
-        font-family: Geneva, Tahoma, Verdana, sans-serif; 
-        font-size: 16px;
-        margin: 10px 0px 30px 0px;
-    }
+    
     </style>
         <main class="container">
             <div class="logo">
