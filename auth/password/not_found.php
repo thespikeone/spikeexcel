7adab02e08ae16054f95b407708994b4777aec9b
THESPIKEONE <ysarni42@gmail.com>
2021-11-07 15:31:16 +0100

    change password with token

---
+++ auth/password/not_found.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</head>
+<body>
+    <center>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>
+                    Oops!</h1>
+                <h2>
+                    404 Not Found</h2>
+                <div class="error-details">
+                    Sorry, an error has occured, your link is invalid!
+                </div>
+                <div class="error-actions">
+                    <a href="../../index.php" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                        Go Home </a><a href="password-forgot.php" class="btn btn-default btn-lg">Request New passowrd </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</center>
+</body>
+</html>
